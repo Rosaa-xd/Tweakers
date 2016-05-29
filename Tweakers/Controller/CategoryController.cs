4c4ff0799ce0a8d5ca5eac2dda60ac36d7447812
Heijkoop, Marie-Rose M.M <m.heijkoop@student.fontys.nl>
2016-05-29 12:53:49 +0200

    Added all controllers without functionality

---
+++ Tweakers/Controller/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tweakers.Controller
+{
+    public class CategoryController : System.Web.Mvc.Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tweakers/Controller/SpecificationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tweakers.Controller
+{
+    public class SpecificationController : System.Web.Mvc.Controller
+    {
+        // GET: Specification
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
