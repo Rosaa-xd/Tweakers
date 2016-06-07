f1603f6e52d3f05a2c4bfa13fa837c586ab904ac
Heijkoop, Marie-Rose M.M <m.heijkoop@student.fontys.nl>
2016-06-07 16:18:42 +0200

    Generate ParentCategory URL's

--- Tweakers/Controllers/CategoryController.cs
+++ Tweakers/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tweakers.Models;
 
 namespace Tweakers.Controllers
 {
@@ -12,6 +13,12 @@ namespace Tweakers.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult PriceWatch()
+        {
+            List<Category> categories = Category.ReturnAllCategories();
+            return View(categories);
         }
     }
 }
--- Tweakers/Models/Category.cs
+++ Tweakers/Models/Category.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Tweakers.Models
 {
-    public class Category
+    public class Category : DbContext
     {
+        private static List<Category> categories = new List<Category>();
+        private static List<Category> parentCategories = new List<Category>();
         public int ID { get; set; }
         public string Name { get; set; }
         public Category ParentCategory { get; set; }
@@ -18,13 +22,9 @@ namespace Tweakers.Models
             Name = name;
         }
 
-        public Category(string name)
+        public Category(int id, string name, Category category)
         {
-            Name = name;
-        }
-
-        public Category(string name, Category category)
-        {
+            ID = id;
             Name = name;
             ParentCategory = category;
         }
@@ -35,11 +35,82 @@ namespace Tweakers.Models
             Products = products;
         }
 
-        public Category(string name, Category category, List<Product> products)
+        public Category(int id, string name, Category category, List<Product> products)
         {
+            ID = id;
             Name = name;
             ParentCategory = category;
             Products = products;
+        }
+
+        public static List<Category> ReturnAllCategories()
+        {
+            string query = "SELECT * " +
+                           "FROM TBL_CATEGORY";
+
+            using (OracleConnection connection = CreateConnection())
+            using (OracleCommand command = new OracleCommand(query, connection))
+            using (OracleDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                     categories.Add(GetCategoryFromDataRecord(reader));
+                }
+            }
+            return categories;
+        }
+
+        public static List<Category> ReturnAllParentCategories()
+        {
+            string query = "SELECT * " +
+                           "FROM TBL_CATEGORY " +
+                           "WHERE CATGORY_ID IS NULL";
+
+            using (OracleConnection connection = CreateConnection())
+            using (OracleCommand command = new OracleCommand(query, connection))
+            using (OracleDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    parentCategories.Add(GetCategoryFromDataRecord(reader));
+                }
+            }
+            return parentCategories;
+        }
+
+        public static Category FindById(int id)
+        {
+            string query = "SELECT * FROM TBL_CATEGORY WHERE ID=:id";
+
+            using (OracleConnection connection = CreateConnection())
+            using (OracleCommand command = new OracleCommand(query, connection))
+            {
+                command.BindByName = true;
+                command.Parameters.Add(new OracleParameter("id", id));
+
+                using (OracleDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return GetCategoryFromDataRecord(reader);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static Category GetCategoryFromDataRecord(IDataRecord record)
+        {
+            if (!Convert.IsDBNull(record["CATEGORY_ID"]))
+            {
+                return new Category(
+                    Convert.ToInt32(record["ID"]),
+                    Convert.ToString(record["NAME"]),
+                    FindById(Convert.ToInt32(record["CATEGORY_ID"])));
+            }
+            return new Category(
+                Convert.ToInt32(record["ID"]),
+                Convert.ToString(record["NAME"]));
         }
     }
 }
